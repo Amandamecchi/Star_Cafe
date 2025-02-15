8ef6b80a3b3f6a22347276d7da521b69061e1c33
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-02-15 09:31:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const cafeRoutes = require("./routes/cafeRoutes")
+//const cafeRoutes = require("./routes/cafeRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
